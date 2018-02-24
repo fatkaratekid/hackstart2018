26c5f21f7aa9aa2d02e309e38d538eae1f8f1654
rasp <rasp@fake>
2018-02-24 11:54:53 +0000

    some basic image manipulation

--- camera.py
+++ camera.py
@@ -1,3 +1,21 @@
 import picamera
-camera = picamera.PiCamera()
-camera.capture('/home/pi/image.jpg')
+from time import sleep
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1280, 720)
+    camera.framerate = 30
+    # Wait for the automatic gain control to settle
+    sleep(2)
+    # Now fix the values
+    camera.shutter_speed = camera.exposure_speed
+    camera.exposure_mode = 'off'
+    g = camera.awb_gains
+    camera.awb_mode = 'off'
+    camera.awb_gains = g
+    camera.contrast = 100
+    picamera.color.Color('green')
+    print('cheese green')
+    camera.capture('/home/pi/image_g.jpg')
+    print('cheese red')
+    camera.capture('/home/pi/image_r.jpg')
+    print('done')
