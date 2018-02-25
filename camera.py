774ca1da043f42204106c85bd6f5d3f860d5d892
rasp <rasp@fake>
2018-02-25 09:06:29 +0000

    simple fallout location

--- camera.py
+++ camera.py
@@ -30,6 +30,28 @@ def detect_red(camera):
 
     return r.sum() > g.sum()
 
+def fallout_location(camera):
+    output = np.empty((320*240*3), dtype=np.uint8)
+    camera.capture(output, 'rgb')
+    output =  output.reshape(240, 320, 3)
+
+    r = np.empty(320*240*3)
+    r =  r.reshape(240, 320, 3)
+    g = np.copy(r)
+
+    r[:,:,0] = output[:,:,0]
+    r[r < treshold] = 0
+
+    g[:,:,1] = output[:,:,1]
+    g[g < treshold] = 0
+
+    fallout_location_g = argmin([sum(g[:80]), sum(g[80:160]), sum(g[160:240])])
+    fallout_location_r = argmax([sum(r[:80]), sum(r[80:160]), sum(r[160:240])])
+    
+    if fallout_location_g == fallout_location_r:
+        return fallout_location_r
+    else:
+        return fallout_location_r
 
 if __name__=="__main__":
     with picamera.PiCamera() as camera:
