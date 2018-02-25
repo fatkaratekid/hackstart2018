3f5f884459cb7792c6cf7cd1e01389980231ca18
Ivan Nesic <fonposta@gmail.com>
2018-02-25 10:17:57 +0100

    added a few more detections

--- camera.py
+++ camera.py
@@ -30,28 +30,46 @@ def detect_red(camera):
 
     return r.sum() > g.sum()
 
-def fallout_location(camera):
+def get_image():
     output = np.empty((320*240*3), dtype=np.uint8)
     camera.capture(output, 'rgb')
-    output =  output.reshape(240, 320, 3)
+    return  output.reshape(240, 320, 3)
 
+def red_matrix(image_matrix):
     r = np.empty(320*240*3)
     r =  r.reshape(240, 320, 3)
-    g = np.copy(r)
 
-    r[:,:,0] = output[:,:,0]
+    r[:,:] = np.squeeze(image_matrix[:,:,0])
     r[r < treshold] = 0
 
-    g[:,:,1] = output[:,:,1]
-    g[g < treshold] = 0
+    return r
 
-    fallout_location_g = argmin([sum(g[:80]), sum(g[80:160]), sum(g[160:240])])
-    fallout_location_r = argmax([sum(r[:80]), sum(r[80:160]), sum(r[160:240])])
+def green_matrix(image_matrix):
+    g = np.empty(320*240*3)
+    g =  g.reshape(240, 320, 3)
+
+    g[:,:] = np.squeeze(image_matrix[:,:,1])
+    g[r < treshold] = 0
+
+    return g
+
+
+def fallout_location(camera):
+    output = get_image()
+
+    r = red_matrix(output)
+    g = green_matrix(output)
     
-    if fallout_location_g == fallout_location_r:
-        return fallout_location_r
-    else:
-        return fallout_location_r
+    matrix_diff = r-g
+    #eliminate white and see where red dominates above green
+    
+    red_domination = argmax([sum(matrix_diff[:80]), sum(matrix_diff[80:160]), sum(matrix_diff[160:240])])
+
+    # fallout_location_g = argmin([sum(g[:80]), sum(g[80:160]), sum(g[160:240])])
+    # fallout_location_r = argmax([sum(r[:80]), sum(r[80:160]), sum(r[160:240])])
+
+    return red_domination
+
 
 if __name__=="__main__":
     with picamera.PiCamera() as camera:
@@ -64,7 +82,9 @@ if __name__=="__main__":
     	g = camera.awb_gains
     	camera.awb_mode = 'off'
     	camera.awb_gains = g
-    
+        
+        init_image_state = get_image()
     	for _ in '.'*20:
+            print(init_image_state - get_image())
             print(detect_red(camera))
             sleep(1)
